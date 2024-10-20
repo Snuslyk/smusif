3ce0338f3f3f8c270295b3b4eff734f19f9b3c33
Snuslyk <127945008+Snuslyk@users.noreply.github.com>
2024-10-20 12:54:56 +0300

    .

--- src/lib/data/artists.ts
+++ src/lib/data/artists.ts
@@ -2,7 +2,6 @@ import ArtiSun from "$lib/assets/artist_avaatars/ArtiSun.png"
 import klorbot from "$lib/assets/artist_avaatars/klorbot.png"
 import OSKENIS from "$lib/assets/artist_avaatars/OSKENIS.jpg"
 import WALKER from "$lib/assets/artist_avaatars/WALKER.jpg"
-import onTomorrow from "$lib/assets/artist_avaatars/onTomorrow.jpg"
 import lazyLion from "$lib/assets/artist_avaatars/LazyLion.jpg"
 
 export let artists = [
@@ -11,5 +10,4 @@ export let artists = [
     ['ВАЛКЕР', WALKER],
     ['klorbot', klorbot],
     ["Lazy Lion", lazyLion],
-    ['отложу на завтра', onTomorrow],
 ];
